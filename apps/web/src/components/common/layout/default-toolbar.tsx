c1ea97cd42e4b7932be49033cac40f39d0138c7b
Kenneth Wußmann <kenneth@wussmann.net>
2026-01-18 02:52:18 +0100

    Use developer only wrapper

--- apps/web/src/components/common/layout/default-toolbar.tsx
+++ apps/web/src/components/common/layout/default-toolbar.tsx
@@ -3,23 +3,24 @@ import { ExternalLink } from 'lucide-react';
 import { Link } from 'react-router';
 import { Button } from '@/components/ui';
 import { settings, ThemeDropdownMenu } from '@/lib';
-import { useDevMode } from '../dev-tools';
+import { DeveloperOnly } from '../dev-tools';
 
 export const DefaultToolbar = () => {
-  const { isDev } = useDevMode();
   const [isApiEnabled] = useAtom(settings.backend.enabled);
   const [httpUrl] = useAtom(settings.backend.httpUrl);
 
   return (
     <div className="flex gap-2">
-      {isDev && isApiEnabled && (
-        <Button variant={'outline'} asChild>
-          <Link to={httpUrl} target={'_blank'}>
-            <ExternalLink />
-            GraphiQL
-          </Link>
-        </Button>
-      )}
+      <DeveloperOnly>
+        {isApiEnabled && (
+          <Button variant={'outline'} asChild>
+            <Link to={httpUrl} target={'_blank'}>
+              <ExternalLink />
+              GraphiQL
+            </Link>
+          </Button>
+        )}
+      </DeveloperOnly>
       <ThemeDropdownMenu />
     </div>
   );
