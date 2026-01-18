5bbb328b9cad729f64e6e73cbc890915bba6cd47
Kenneth Wußmann <kenneth@wussmann.net>
2026-01-18 03:30:08 +0100

    Add status indicator to default toolbar

--- apps/web/src/components/common/layout/default-toolbar.tsx
+++ apps/web/src/components/common/layout/default-toolbar.tsx
@@ -2,15 +2,18 @@ import { useAtom } from 'jotai';
 import { ExternalLink } from 'lucide-react';
 import { Link } from 'react-router';
 import { Button } from '@/components/ui';
-import { settings, ThemeDropdownMenu } from '@/lib';
+import { settings, ThemeDropdownMenu, useServerStatus } from '@/lib';
 import { DeveloperOnly } from '../dev-tools';
+import { StatusIndicator } from '../status-indicator';
 
 export const DefaultToolbar = () => {
   const [isApiEnabled] = useAtom(settings.backend.enabled);
   const [httpUrl] = useAtom(settings.backend.httpUrl);
+  const { status } = useServerStatus();
 
   return (
     <div className="flex gap-2">
+      {status === 'offline' && <StatusIndicator status={status} />}
       <DeveloperOnly>
         {isApiEnabled && (
           <Button variant={'outline'} asChild>
